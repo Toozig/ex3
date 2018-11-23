f582709e7be01f1668b705111751d76233dfb3ce
Toozig <35314420+Toozig@users.noreply.github.com>
2018-11-23 23:16:31 +0200

    3rd update

--- Parser.c
+++ Parser.c
@@ -10,9 +10,11 @@
 
 #define GROW_FACTOR 10
 
-size_t ParseNumber(const char *infix, Bullet *const *bullets, size_t *numOfBullets, size_t i);
+size_t ParseNumber(const char *infix, Bullet **bullets, size_t *numOfBullets, size_t i);
 
-size_t *leftParenthesisFunc(Bullet **bullets, size_t *numOfBullets, const Stack *stack);
+size_t *leftParenthesisFunc(Bullet **bullets, size_t *numOfBullets, Stack *stack);
+
+size_t *addOperator(Bullet **bullets, size_t *numOfBullets, Stack *stack, Bullet *bullet);
 
 Bullet *createBullet(int data, int type)
 {
@@ -55,82 +57,7 @@ Bullet *createOperator(char operator)
     return createBullet(data, OPERATOR);
 }
 
-int ParseInfix(char *infix, Bullet **bullets, size_t *numOfBullets)
-{
-    Stack *stack = stackAlloc(sizeof(Bullet));
-    if (stack == NULL)
-    {
-        fprintf(stderr, "ERROR memory alloc problem");
-        return 1;
-    }
-    size_t i;
-    for (i = 0; i < strlen(infix); i++)
-    {
-        if (infix[i] == ' ')
-        {
-            continue;
-        }
-        else if (isdigit(infix[i]))
-        {
-            i = ParseNumber(infix, bullets, numOfBullets, i);
-        }
-        else if (infix[i] == '(')
-        {
-            Bullet *bullet = createBullet(0, RIGHT_PARENTHESIS);
-            if (bullet == NULL)
-            {
-                return 1;
-            }
-            push(stack, bullet);
-        }
-        else if ( infix[i] == ')')
-        {
-            numOfBullets = leftParenthesisFunc(bullets, numOfBullets, stack);
-        }
-        else
-        {
-            Bullet *bullet = createOperator(infix[i]);
-            if( bullet == NULL)
-            {
-                return 0;
-            }
-            if (isEmptyStack(stack))
-            {
-                push(stack,bullet);
-            }
-            Bullet *topElement =  stack->_top->_data;
-            if(topElement->type == LEFT_PARENTHESIS)
-            {
-                push(stack,bullet);
-            } else
-            {
-                while (!isEmptyStack(stack) && topElement->type != '(' ) //todo another condition that i did not understand
-            }
-
-        }
-
-
-
-    }
-}
-
-size_t *leftParenthesisFunc(Bullet **bullets, size_t *numOfBullets, const Stack *stack)
-{
-    while(isEmptyStack(stack))
-            {
-                Bullet *bullet = pop(stack);
-                if(bullet->data == '(')
-                {
-                    free(bullet);
-                    break;
-                }
-                bullets[*numOfBullets] = bullet;
-                numOfBullets++;
-            }
-    return numOfBullets;
-}
-
-size_t ParseNumber(const char *infix, Bullet *const *bullets, size_t *numOfBullets, size_t i)
+size_t ParseNumber(const char *infix, Bullet **bullets, size_t *numOfBullets, size_t i)
 {
     int num;
     sscanf(&infix[i], "%d", &num);
@@ -148,6 +75,132 @@ size_t ParseNumber(const char *infix, Bullet *const *bullets, size_t *numOfBulle
 
     }
     bullets[*numOfBullets] = bullet;
-    numOfBullets++;
+
     return i;
 }
+
+int ParseInfix(char *infix, Bullet **bullets, size_t *numOfBullets)
+{
+    Stack *stack = stackAlloc(sizeof(Bullet));
+    if (stack == NULL)
+    {
+        fprintf(stderr, "ERROR memory alloc problem");
+        return 1;
+    }
+    size_t i;
+    for (i = 0; i < strlen(infix); i++)
+    {
+        if (isdigit(infix[i]))
+        {
+            i = ParseNumber(infix, bullets, numOfBullets, i);
+            numOfBullets++;
+        } else if (infix[i] == ' ')
+        {
+            continue;
+        } else if (infix[i] == '(')
+        {
+            Bullet *bullet = createBullet(0, RIGHT_PARENTHESIS);
+            if (bullet == NULL)
+            {
+                return 1;
+            }
+            push(stack, bullet);
+        } else if (infix[i] == ')')
+        {
+            numOfBullets = leftParenthesisFunc(bullets, numOfBullets, stack);
+        } else
+        {
+            Bullet *bullet = createOperator(infix[i]);
+            if (bullet == NULL)
+            {
+                return 0;
+            }
+            numOfBullets = addOperator(bullets, numOfBullets, stack, bullet);
+        }
+    }
+    while (!isEmptyStack(stack))
+    {
+        Bullet *bullet = pop(stack);
+        bullets[*numOfBullets] = bullet;
+        numOfBullets++;
+    }
+}
+
+/**
+ * This function checks if the precedence of the operator is bigger to the precedence of thetop elemnt
+ * @param operator the operator
+ * @param top the top element of the stack
+ * @return  0 if the operator precedence is bigger than the top element. 1 otherwise
+ */
+int precedenceIsGreater(Bullet *operator, Bullet *top)
+{
+    if (operator->type == top->type)
+    {
+       switch (operator->data)
+       {
+           case MULTIPLICATION:
+           case DIVISION:
+               if(top->data == MINUS || top->data == ADDITION)
+               {
+                   return 0;
+               }
+               break;
+           case POWER:
+               if(top->data != POWER)
+               {
+                   return 0;
+               }
+           default:
+               break;
+       }
+    }
+    return 1;
+}
+
+size_t *addOperator(Bullet **bullets, size_t *numOfBullets, Stack *stack, Bullet *bullet)
+{
+    if (isEmptyStack(stack))
+    {
+        push(stack, bullet);
+    }
+    Bullet *topElement = stack->_top->_data;
+    if (topElement->type == LEFT_PARENTHESIS)
+    {
+        push(stack, bullet);
+    } else
+    {
+        while (!isEmptyStack(stack) &&
+               topElement->type != '(' && precedenceIsGreater(bullet, topElement)) //todo check if this function work
+        {
+            Bullet *poped = pop(stack);
+            bullets[*numOfBullets] = poped;
+            numOfBullets++;
+            topElement = stack->_top->_data;
+        }
+        push(stack, bullet);
+    }
+    return numOfBullets;
+}
+
+/**
+ * This function preform the part of the algorithm if a left parenthesis is met
+ */
+size_t *leftParenthesisFunc(Bullet **bullets, size_t *numOfBullets, Stack *stack)
+{
+    while (isEmptyStack(stack))
+    {
+        Bullet *bullet = pop(stack);
+        if (bullet->data == '(')
+        {
+            free(bullet);
+            break;
+        }
+        bullets[*numOfBullets] = bullet;
+        numOfBullets++;
+    }
+    return numOfBullets;
+}
+
+/**
+ * this function parse a digit char in the given input
+ */
